d85553c331dd8135c3fd5f3aaaa4a62bb26e582f
Nanai10a <Nanai10a@outlook.jp>
2021-09-03 03:05:20 +0900

    🚧 `Deref(Mut)?`への対応.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ pub trait ScopeFn {
     fn let_mut<F: FnOnce(&mut Self) -> R, R>(&mut self, f: F) -> R;
     // cannot use `DerefMove`, using `Box`.
     fn let_move_ptr<F: FnOnce(Box<Self>) -> R, R>(self: Box<Self>, f: F) -> R;
+    fn let_ref_ptr<F: FnOnce(&Target) -> R, R, Target>(&self, f: F) -> R
+    where Self: ::std::ops::Deref<Target = Target>;
+    fn let_mut_ptr<F: FnOnce(&mut Target) -> R, R, Target>(&mut self, f: F) -> R
+    where Self: ::std::ops::DerefMut<Target = Target>;
 }
 
 impl<T> ScopeFn for T {
@@ -19,4 +23,14 @@ impl<T> ScopeFn for T {
     fn let_mut<F: FnOnce(&mut Self) -> R, R>(&mut self, f: F) -> R { f(self) }
 
     fn let_move_ptr<F: FnOnce(Box<Self>) -> R, R>(self: Box<Self>, f: F) -> R { f(self) }
+
+    fn let_ref_ptr<F: FnOnce(&Target) -> R, R, Target>(&self, f: F) -> R
+    where Self: ::std::ops::Deref<Target = Target> {
+        f(self)
+    }
+
+    fn let_mut_ptr<F: FnOnce(&mut Target) -> R, R, Target>(&mut self, f: F) -> R
+    where Self: ::std::ops::DerefMut<Target = Target> {
+        f(self)
+    }
 }
